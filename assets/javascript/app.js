978e67420133bad524d9699e130b9c2b31475a95
Abe <abrahan.rodri@gmail.com>
2019-07-11 19:22:33 -0400

    working on javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,53 @@ var topics = ["cat",
 "dolphin",
 "bear"];
 var addedTopics = "";
+
+$("button").on("click", function() {
+    // Grabbing and storing the data-animal property value from the button
+    var animal = $(this).attr("data-animal");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "TcbGhYWRBlW2REvL6nD4Co0xKVOoIHZN";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+          var animalDiv = $("<div>");
+
+          var p = $("<p>").text("Rating: " + results[i].rating);
+
+          var animalImage = $("<img>");
+          
+          animalImage.attr("src", results[i].images.fixed_height.url);
+
+         
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
+
+         
+          $("#gifs-appear-here").prepend(animalDiv);
+        }
+      });
+  });
+
+$(".gif").on("click", function() {
+  
+    var state = $(this).attr("data-state");
+    
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
