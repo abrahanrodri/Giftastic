b904d461c5da2399b2ce93a7443c6e07eeb010ad
Abe <abrahan.rodri@gmail.com>
2019-07-24 00:52:19 -0400

    getting the buttons to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,13 +13,13 @@ var animals = ["cat",
 $(document).ready(function() {
 
   function showButtons() {
+
     for (var i = 0; i < animals.length; i++) {
       var newAnimals = $("<button>");
-      newAnimals.addClass("btn btn-default topicBtn");
-      newAnimals.attr("type", "button");
+      newAnimals.addClass("btn btn-primary");
+      newAnimals.attr("data-name", animals[i]);
       newAnimals.append(animals[i]);
-      newAnimals.attr("value", animals[i]);
-      $("#newButtonDiv").append(newAnimals);
+      $("button").append(newAnimals);
 
       
     }
@@ -27,9 +27,11 @@ $(document).ready(function() {
  
   
   showButtons();
+  showGifs();
 
 function showGifs(animal) {
     
+  var animal = $(this).attr("data-name");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
     + animal + "&api_key=TcbGhYWRBlW2REvL6nD4Co0xKVOoIHZN&q=&limit=10&offset=0&rating=PG-13&lang=en";
@@ -38,11 +40,28 @@ function showGifs(animal) {
       url: queryURL,
       method: "GET"
     }).done(function(response) {
-        
+         var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+                var gifDiv = $('.gifs');
+
+                var gifShow = $('<img>');
+
+                gifShow.attr('src', results[i].images.fixed_height_still.url);
+                gifShow.attr('title', "Rating: " + results[i].rating);
+                gifShow.attr('data-still', results[i].images.fixed_height_still.url);
+                gifShow.attr('data-state', 'still');
+                gifShow.addClass('gif');
+                gifShow.attr('data-animate', results[i].images.fixed_height.url);
+                gifDiv.append(gifShow);
+                $(".gifs").append(gifDiv);
+            }
+
+        });
+    }
         
 
     });
 
-  }
 
-})
