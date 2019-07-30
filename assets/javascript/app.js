88434d0bcb5662520f1aec4e9006217d70848f77
Abe <abrahan.rodri@gmail.com>
2019-07-30 19:52:42 -0400

    got it working (finally). Just need to add Bootstrap to it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,76 @@
 
- var animals = ["penguin", "dog", "cat", "tiger", "falcon", "eagle", "hamster"];
+var animals = ["penguin", "dog", "cat", "tiger", "falcon", "eagle", "hamster"];
 
 
- function renderButtons() {
+function renderButtons() {
 
-  
-   $("#buttons-view").empty();
+  $("#buttons-view").empty();
 
+  for (var i = 0; i < animals.length; i++) {
 
-   for (var i = 0; i < animals.length; i++) {
+    var a = $("<button>");
 
-     
-     var a = $("<button>");
-  
-     a.addClass("animal");
-    
-     a.attr("data-name", animals[i]);
-    
-     a.text(animals[i]);
-     
-     $("#buttons-view").append(a);
-     
-   }
- }
+    a.addClass("animal");
+    a.attr("data-name", animals[i]);
+    a.text(animals[i]);
+    $("#buttons-view").append(a);
 
- 
- $("#add-animal").on("click", function(event) {
-  
-   event.preventDefault();
+  }
+}
 
- 
-   var animal = $("#animal-input").val().trim();
+$("#add-animal").on("click", function (event) {
 
-   animals.push(animal);
+  event.preventDefault();
 
-   
-   renderButtons();
- });
+  var animal = $("#animal-input").val().trim();
 
- 
- renderButtons();
+  animals.push(animal);
+
+  renderButtons();
+});
+
+function displayGifs() {
+
+  var rap = $(this).attr("data-name");
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      rap + "&api_key=TcbGhYWRBlW2REvL6nD4Co0xKVOoIHZN&limit=10";
+
+  $.ajax({ url: queryURL, method: "GET" })
+  .done(function (response) {
+
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+
+          var gifDiv = $('.tenGifs');
+          var showGif = $('<img>');
+
+          showGif.attr('src', results[i].images.fixed_height_still.url);
+          showGif.attr('title', "Rating: " + results[i].rating);
+          showGif.attr('data-still', results[i].images.fixed_height_still.url);
+          showGif.attr('data-state', 'still');
+          showGif.addClass('gif');
+          showGif.attr('data-animate', results[i].images.fixed_height.url);
+          gifDiv.append(showGif)
+          $(".tenGifs").append(gifDiv);
+      }
+
+  });
+}
+
+$(document).on('click', '.gif', function () {
+  var state = $(this).attr('data-state');
+
+  if (state === 'still') {
+
+      $(this).attr('src', $(this).data('animate'));
+      $(this).attr('data-state', 'animate');
+  } else {
+      $(this).attr('src', $(this).data('still'));
+      $(this).attr('data-state', 'still');
+  };
+});
+
+$(document).on("click", ".animal", displayGifs);
+renderButtons();
